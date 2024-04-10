f8ed2371930c088607cd19c4f563df135d9c4b49
leftkats <left.katsinelos@gmail.com>
2024-04-10 12:27:21 +0300

    Refactor

--- airdrop/multisender.py
+++ airdrop/multisender.py
@@ -3,70 +3,65 @@ import json
 from datetime import datetime
 from subprocess import PIPE, Popen
 
-DAB_TOKEN_ADDRESS = 'BBfACm5eg8CWmcRmgcn1c2uzN1fGhvQ1b8iDR92uaQVT'
-
-token_address = DAB_TOKEN_ADDRESS
+mainnet_token_address = 'BBfACm5eg8CWmcRmgcn1c2uzN1fGhvQ1b8iDR92uaQVT'
 recipients_path = 'airdrop/recipients.txt'
 day_key = datetime.now().strftime("%Y-%m-%d")
 
 
-def multisend(
+def send(
     token_address: str,
-    recipients_path: str,
-    day_key: datetime,
+    recipient_address: str,
+    amount: str,
 ):
-    logs_succeed_path, mode = f'airdrop/airdrop-{day_key}-logs-succeed.json', 'w+'
-    if os.path.exists(logs_succeed_path):
-        mode = 'a+'
-    log_file_succeed = open(logs_succeed_path, mode)
+    command = f"spl-token transfer --fund-recipient \
+                {token_address} \
+                {amount} \
+                {recipient_address}"
+    process = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
 
-    logs_failed_path, mode = f'airdrop/airdrop-{day_key}-logs-failed.json', 'w+'
-    if os.path.exists(logs_failed_path):
-        mode = 'a+'
-    log_file_failed = open(logs_failed_path, mode)
-
-    recipients = open(recipients_path, 'r+')
-    lines = recipients.readlines()
-    for i, _ in enumerate(lines):
-        # Transfer
-        recipient_address = lines[i].strip().split(',')[0]
-        amount = lines[i].strip().split(',')[1]
-
-        command = f"spl-token transfer --fund-recipient --allow-unfunded-recipient\
-                    {token_address} \
-                    {amount} \
-                    {recipient_address}"
-        process = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
-        stdout, stderr = process.communicate()
-
-        # Log entry
-        if stdout:
-            status = 'success'
-            file_to_write = log_file_succeed
-        else:
-            status = 'failed'
-            file_to_write = log_file_failed
-        writetolog(
-            status,
-            token_address,
-            recipient_address,
-            stdout,
-            stderr,
-            file_to_write,
-        )
-        print("i: ", i)
-
-    recipients.close()
-    log_file_succeed.close()
-    # We don't want to have an empty file.
-    if os.path.getsize(logs_failed_path) == 0:
-        os.remove(logs_failed_path)
-        # End the program
+    # Log entry
+    if stdout:
+        status = 'success'
+        file_to_write = log_file_succeed
     else:
-        # 1. Read the failed file and get the recipients
-        # 2. Call again the multisend() with the new recipients
-        # 3. Do this until we have no failed file anymore -> this is happening already here
-        log_file_failed.close()
+        status = 'failed'
+        file_to_write = log_file_failed
+    writetolog(
+        status,
+        token_address,
+        recipient_address,
+        stdout,
+        stderr,
+        file_to_write,
+    )
+
+
+def get_paid_addresses():
+    paid_addresses = []
+    try:
+        with open('airdrop/paid.ndjson', 'r+') as paid:
+            rows = paid.readlines()
+            for row in rows:
+                row = json.loads(row)
+                paid_addresses.append(row['address'])
+    except FileNotFoundError:
+        print('Paid addresses file not found!')
+    return paid_addresses
+
+
+def get_recipients():
+    recipients_new = []
+    with open(recipients_path, 'r+') as recipients:
+        rows = recipients.readlines()
+        for i, _ in enumerate(rows):
+            recipient_address = rows[i].strip().split(',')[0]
+            amount = rows[i].strip().split(',')[1]
+            recipients_new.append({
+                'address': recipient_address,
+                'amount': amount
+            })
+    return recipients_new
 
 
 def writetolog(
@@ -84,7 +79,7 @@ def writetolog(
         reciever_addr=recipient_address,
         logs=stdout.decode('utf-8') if stdout else stderr.decode('utf-8')
     )
-    print("logs: ", log_entry)
+    print(log_entry)
     print(json.dumps(
         log_entry,
         ensure_ascii=True),
@@ -93,4 +88,23 @@ def writetolog(
     )
 
 
-multisend(token_address, recipients_path, day_key)
+if __name__ == 'main':
+    # Read data
+    paid = get_paid_addresses()
+    recipients = get_recipients()
+    # Create log files
+    logs_succeed_path, mode = f'airdrop/airdrop-{day_key}-logs-succeed.json', 'w+'
+    if os.path.exists(logs_succeed_path):
+        mode = 'a+'
+    log_file_succeed = open(logs_succeed_path, mode)
+
+    logs_failed_path, mode = f'airdrop/airdrop-{day_key}-logs-failed.json', 'w+'
+    if os.path.exists(logs_failed_path):
+        mode = 'a+'
+    log_file_failed = open(logs_failed_path, mode)
+    # Tranfer tokens to recipients
+    for recipient in recipients:
+        if recipient['address'] in paid:
+            print(f'Found an already paid address --> {recipient["address"]}')
+            continue
+        send(mainnet_token_address, recipient["address"], recipient['amount'])
